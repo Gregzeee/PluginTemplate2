48cffc982e7ef756a50b940e8cc106e32927f596
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2025-07-20 12:48:37 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,15 +18,12 @@ java {
 
 dependencies {
     compileOnly(libs.paper.api)
-    compileOnly(libs.commandapi)
-    compileOnly(libs.configlib.yaml)
-    compileOnly(libs.configlib.paper)
     compileOnly(libs.lombok)
     annotationProcessor(libs.lombok)
 }
 
-group = "xyz.strealex.pluginname"
-version = "0.0.1-SNAPSHOT"
+group = "me.gregzee.pluginname"
+version = "1.0.0"
 description = "Plugin Description"
 
 tasks {
@@ -58,12 +55,10 @@ tasks {
 
     processResources {
         inputs.property("version", project.version)
-        inputs.property("configlibVersion", libs.versions.configlib.get())
 
         filesMatching("plugin.yml") {
             expand(
                 "version" to rootProject.version,
-                "configlibVersion" to libs.versions.configlib.get()
             )
         }
     }
@@ -90,7 +85,6 @@ tasks {
         }
 
         downloadPlugins {
-            url("https://github.com/CommandAPI/CommandAPI/releases/download/10.0.1/CommandAPI-10.0.1.jar")
             url("https://github.com/EssentialsX/Essentials/releases/download/2.21.1/EssentialsX-2.21.1.jar")
             url("https://download.luckperms.net/1587/bukkit/loader/LuckPerms-Bukkit-5.5.2.jar")
             url("https://ci.lucko.me/job/spark/487/artifact/spark-bukkit/build/libs/spark-1.10.138-bukkit.jar")
